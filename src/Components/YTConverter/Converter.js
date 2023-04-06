0df28445c82f37791d5e0b6085aead515f4d3164
Zurab Zaridze <zaridzezuri@gmail.com>
2023-04-06 13:47:05 +0400

    Add error handling for rate limit & add error texts

--- src/Components/YTConverter/Converter.js
+++ src/Components/YTConverter/Converter.js
@@ -8,21 +8,14 @@ import DownArrow from "./images/down-arrow.png";
 
 function Converter() {
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("")
+  const [error, setError] = useState("");
   const [userInput, setUserInput] = useState("");
   const [downloadLink, setDownloadLink] = useState("");
   const [downloadName, setDownloadName] = useState("");
 
-  const reset = () => {
-    setUserInput("");
-    setDownloadLink("");
-    setDownloadName("");
-    setError("")
-  };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setError("")
+    setError("");
     setLoading(true);
 
     if (isYTLinkValid(userInput)) {
@@ -32,49 +25,58 @@ function Converter() {
         setDownloadLink(res.link);
         setDownloadName(res.title);
         setLoading(false);
+
+        if (res.status == "ok") {
+          setUserInput("")
+          setError("")
+        }
+
+        if (res.message.includes("quota")) {
+          // Rate Limit
+          setError("გადმოწერის დღიური ლიმიტი ამოიწურა");
+        }
       });
     }
 
     if (!isYTLinkValid(userInput)) {
-      setError("ბმული არავალიდურია")
+      setError("ბმული არავალიდურია");
+      setLoading(false);
     }
   };
 
   useEffect(() => {
-    userInput == "" && setError("")
-    isYTLinkValid(userInput) && setError("")
-  }, [userInput])
+    userInput == "" && setError("");
+    isYTLinkValid(userInput) && setError("");
+  }, [userInput]);
 
   return (
     <div className={styles.Container}>
       <form onSubmit={(e) => handleSubmit(e)} className={styles.FirstRow}>
-        {!downloadName && (
-          <>
-            <div style={{ marginBottom: "0.5rem" }}>
-              <label className={styles.Label}>შეიყვანე ბმული</label>
-              <img className={`${styles.arrow} lightIMG`} src={DownArrow} alt="Arrow" />
-            </div>
+        <div style={{ marginBottom: "0.5rem" }}>
+          <label className={styles.Label}>შეიყვანე ბმული</label>
+          <img className={`${styles.arrow} lightIMG`} src={DownArrow} alt="Arrow" />
+        </div>
 
-            <input
-              style={error ? {borderBottomColor: "#7a0000"} : {}}
-              className={userInput ? `${styles.Input} ${styles.Opacity1}` : styles.Input}
-              onChange={(e) => setUserInput(e.target.value)}
-              value={userInput}
-            />
-          </>
-        )}
-        <button className={styles.Button} onClick={downloadName ? reset : (e) => handleSubmit(e)}>
+        <input
+          style={error ? { borderBottomColor: "#7a0000" } : {}}
+          className={userInput ? `${styles.Input} ${styles.Opacity1}` : styles.Input}
+          onChange={(e) => setUserInput(e.target.value)}
+          value={userInput}
+        />
+        <button className={styles.Button} onClick={(e) => handleSubmit(e)}>
           {downloadName ? "დააკონვერტირე ხელახლა" : "კონვერტირება"}
         </button>
       </form>
 
-      {downloadLink && <button className={styles.Download}><a href={downloadLink}><b>გადმოწერე</b> <br />{downloadName}</a></button>}
+      {downloadLink && (
+        <button className={styles.Download}>
+          <a href={downloadLink}><b>გადმოწერე</b> <br />{downloadName}</a>
+        </button>
+      )}
 
-      {/* {loading && (
-        <p style={{ color: "white" }}>
-          <br /> მიმდინარეობს კონვერტაცია...
-        </p>
-      )} */}
+      <p style={loading ? {visibility: "visible"} : {}} className={styles.message}>{loading && "მუშავდება..."}</p>
+
+      <p style={error ? {visibility: "visible"} : {}} className={styles.message}>{error && error}</p>
     </div>
   );
 }
