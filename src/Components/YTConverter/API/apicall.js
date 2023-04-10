7621860836b60d8afadc09e244425d36b8ef09b0
Zurab Zaridze <zaridzezuri@gmail.com>
2023-04-10 22:25:26 +0400

    Change old fetch syntax to async/await syntax in apicall

--- src/Components/YTConverter/API/apicall.js
+++ src/Components/YTConverter/API/apicall.js
@@ -1,13 +1,14 @@
 // API - https://rapidapi.com/ytjar/api/youtube-mp36
 import { options } from "./options";
 
-export const apicall = (id) => {
+export const apicall = async (id) => {
   const api = `https://youtube-mp36.p.rapidapi.com/dl?id=${id}`;
 
-  return fetch(api, options)
-    .then((response) => response.json())
-    .then((response) => {
-      return response;
-    })
-    .catch((err) => console.error(err));
+  try {
+    const response = await fetch(api, options);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 };
